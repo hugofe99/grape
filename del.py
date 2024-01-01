bc1e214860e5acf96f13a052b8540db26ab611f6
hugofe99 <fernstrom.hugo@gmail.com>
2024-01-01 16:26:57 +0100

    SemanticScholar demo

--- del.py
+++ del.py
@@ -1,20 +1,28 @@
 import arxiv
 from semanticscholar import SemanticScholar
+from pprint import pprint 
 
 
-client = arxiv.Client()
+# client = arxiv.Client()
 
-search = arxiv.Search(
-  query = "Playing Atari with Deep Reinforcement Learning",
-  max_results = 1,
-  sort_by = arxiv.SortCriterion.Relevance
-)
+# search = arxiv.Search(
+#   query = "Playing Atari with Deep Reinforcement Learning",
+#   max_results=3,
+#   sort_by=arxiv.SortCriterion.Relevance
+# )
 
-results = list(client.results(search))
-result = results[0]
-id = result
+# result = list(client.results(search))[0]
+# id = result.get_short_id().split('v')[0]
+# print(f'{id=}')
 
 sch = SemanticScholar()
-papers = sch.get_paper()
-for r in papers:
-    print(r)
+pag_res = sch.search_paper(
+    query="Playing Atari with Deep Reinforcement Learning",
+    limit=1
+)
+top_paper_id = pag_res[0]["paperId"]
+top_paper = sch.get_paper(top_paper_id)
+for ref in top_paper.references:
+    print(ref['title'])
+
+
