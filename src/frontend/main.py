bcd49e4e07deaa0768e52e74a320a6905ea40273
hugofe99 <fernstrom.hugo@gmail.com>
2024-01-02 02:44:31 +0100

    demo

--- src/frontend/main.py
+++ src/frontend/main.py
@@ -1,25 +1,22 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-from src.backend.trivial_graph import get_trivial_graph_html
-from src.backend.pdf_to_text import pdf_to_text, text_to_reference_section
+from src.backend.pdf import pdf_to_text
+from src.backend.papers.paper import Paper
+from src.backend.graphs.network import PaperNetwork, NetworkConstants
 
+from src.frontend.resources.backend import paper_title_to_network_html
 
 st.title("🍇 Grape")
 
-st.header("PDF Text Extractor")
+st.header("Paper insights")
 pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
 if pdf_file is not None:
-    with st.spinner("Extracting text..."):
-        text = pdf_to_text(pdf_file)
-    ref_sec = text_to_reference_section(text)
-    st.subheader("Reference section?")
-    st.text(ref_sec)
-
-
-st.markdown(
-    """ ___ """
-)
-
-components.html(get_trivial_graph_html(), height=500)
+    with st.spinner("Analysing paper"):
+        title = pdf_to_text.extract_title_from_pdf(pdf_file)
+        html = paper_title_to_network_html(title)
+        st.markdown(f"### {title}")
+        components.html(
+            html, height=NetworkConstants._HEIGHT_INT, width=NetworkConstants._WIDTH_INT
+        )
