8f0a033a5da4a428696674518a63153671b5fce2
hugofe99 <fernstrom.hugo@gmail.com>
2024-01-02 15:57:30 +0100

    added to_agraph_component method

--- src/backend/graphs/network.py
+++ src/backend/graphs/network.py
@@ -1,42 +1,23 @@
 from pyvis.network import Network
 import networkx as nx
 from src.backend.papers.paper import Paper
+from src.backend.graphs.config import get_default_network, NetworkConstants
 from semanticscholar.Paper import Paper as SemanticScholarPaper
-
-
-class NetworkConstants:
-    HEIGHT = "750px"
-    WIDTH = "750px"
-    DIRECTED = False
-    NOTEBOOK = False
-    BACKGROUND_COLOUR = "#0E1117"
-    FONT_COLOUR = "white"
-
-    PAPER_SIZE = 20
-    REF_SIZE = 10
-
-    _HEIGHT_INT = int(HEIGHT.split("px")[0])
-    _WIDTH_INT = int(WIDTH.split("px")[0])
-
-
-def get_default_network(
-    _network_constants: NetworkConstants = NetworkConstants(),
-) -> Network:
-    return Network(
-        height=_network_constants.HEIGHT,
-        width=_network_constants.WIDTH,
-        directed=_network_constants.DIRECTED,
-        notebook=_network_constants.NOTEBOOK,
-        bgcolor=_network_constants.BACKGROUND_COLOUR,
-        font_color=_network_constants.FONT_COLOUR,
-    )
+from streamlit_agraph import agraph, Config, Node, Edge, ConfigBuilder
+from streamlit.components.v1.components import CustomComponent
 
 
 class PaperNetwork:
-    def __init__(self, paper: Paper, _network: Network | None = None) -> None:
+    def __init__(
+        self,
+        paper: Paper,
+        _network: Network | None = None,
+        _network_constants: NetworkConstants = NetworkConstants,
+    ) -> None:
         if not _network:
-            _network = get_default_network()
+            _network = get_default_network(_network_constants)
 
+        self.network_constants = _network_constants
         self.paper = paper
         self.network = _network
         self.html: str = ""
@@ -80,7 +61,7 @@ class PaperNetwork:
             n_id=self.paper.paperId,
             label=self.paper.title,
             title=self._paper_to_description(self.paper),
-            size=NetworkConstants.PAPER_SIZE,
+            size=self.network_constants.PAPER_SIZE,
         )
 
         for ref in self.paper.get_references():
@@ -88,9 +69,35 @@ class PaperNetwork:
                 n_id=ref.paperId,
                 label=f"{ref.title[:10]}...",
                 title=self._paper_to_description(ref),
-                size=NetworkConstants.REF_SIZE,
+                size=self.network_constants.REF_SIZE,
             )
-            self.network.add_edge(source=ref.paperId, to=self.paper.paperId)
+            self.network.add_edge(
+                source=ref.paperId,
+                to=self.paper.paperId,
+                # label='is_ref_to'
+            )
+
+    def to_agraph_component(self, custom_config: bool = False) -> CustomComponent:
+        nodes, edges, _, _, _, _ = self.network.get_network_data()
+
+        def format_node(node_dict: dict) -> dict:
+            return node_dict
+
+        def format_edge(edge_dict: dict) -> dict:
+            edge_dict["source"] = edge_dict.pop("from")
+            edge_dict["target"] = edge_dict.pop("to")
+            return edge_dict
+
+        nodes = [Node(**format_node(node)) for node in nodes]
+        edges = [Edge(**format_edge(edge)) for edge in edges]
+
+        if custom_config:
+            config_builder = ConfigBuilder(nodes, edges)
+            config = config_builder.build()
+        else:
+            config = Config()
+
+        return agraph(nodes, edges, config)
 
     def to_networkx_graph(self) -> nx.Graph:
         """Could be useful for obsidian integration?"""
